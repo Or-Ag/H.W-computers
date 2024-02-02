7db7870d4f9abe2ba43a8492f107e7475abd5850
Or Agasi <67040075+Or-Ag@users.noreply.github.com>
2024-02-02 11:01:50 +0200

    Update answer number 2.java

--- random/H.W - 1.2.24/answer number 2.java
+++ random/H.W - 1.2.24/answer number 2.java
@@ -1,16 +1,14 @@
-import java.util.Random;
 
-public class Main {
-    public static void main(String[] args) {
-        Random random = new Random();
-        int countOfSixes = 0;
+import  java.util.* ;
+public class Main
+{
+    public static void main ( String[ ]  args ) {
+        int mone = 0;
 
-        for (double i = 0; i < 1; i++) {
-            double randomNumber = random.nextInt(6) + 1;
-            System.out.print(randomNumber + " ");
-            countOfSixes += (randomNumber == 6) ? 1 : 0;
+        Random rnd = new Random();
+        for (int i = 1; i <= 1; i++) {
+            double num = 9 + rnd.nextDouble(99);
+            System.out.println(num);
         }
-
-        System.out.println("\nOccurrences of the number 6: " + countOfSixes);
     }
 }
