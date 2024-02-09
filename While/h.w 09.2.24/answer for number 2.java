8909a8cd0813c3a4d8cd2bb82e2933610812f9f1
Or Agasi <67040075+Or-Ag@users.noreply.github.com>
2024-02-09 11:29:14 +0200

    Update answer for number 2.java

--- While/h.w 09.2.24/answer for number 2.java
+++ While/h.w 09.2.24/answer for number 2.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int sum = 0;
+        double sum = 0.0;
+
+        System.out.print("Series: ");
         for (int i = 1; i <= 100; i++) {
-            System.out.print("1");
+            sum += 1.0 / i;
+            System.out.print("1/" + i);
             if (i != 100) {
                 System.out.print(" + ");
             }
-            sum += i;
         }
+
         System.out.println("\nSum: " + sum);
     }
 }
-
